31d83476289ef8963ba6bdd6ae9f027939157875
CosmicWatcher <aryan.kh88@gmail.com>
2024-10-02 18:57:34 -0400

    multiple cors origins

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { cleanEnv, host, num, port, str, testOnly } from "envalid";
+import { cleanEnv, host, num, port, str, json, testOnly } from "envalid";
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ export const env = cleanEnv(process.env, {
   }),
   HOST: host({ devDefault: testOnly("localhost") }),
   PORT: port({ devDefault: testOnly(3000) }),
-  CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:3000") }),
+  CORS_ORIGIN: json(),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
   SUPABASE_ANON_KEY: str(),
