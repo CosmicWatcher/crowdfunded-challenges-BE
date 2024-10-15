9170c6aded2d308251b8683892f8fa3cde6cb197
CosmicWatcher <aryan.kh88@gmail.com>
2024-10-15 11:26:41 -0400

    db types

--- src/common/utils/supabase.ts
+++ src/common/utils/supabase.ts
@@ -1,3 +1,4 @@
+import { Database } from "@/common/types/database.types";
 import { env } from "@/common/utils/envConfig";
 import {
   createServerClient,
@@ -7,19 +8,23 @@ import {
 import type { Request, Response } from "express";
 
 export const createClient = (req: Request, res: Response) => {
-  return createServerClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY, {
-    cookies: {
-      getAll() {
-        return parseCookieHeader(req.headers.cookie ?? "");
-      },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) =>
-          res.appendHeader(
-            "Set-Cookie",
-            serializeCookieHeader(name, value, options),
-          ),
-        );
+  return createServerClient<Database>(
+    env.SUPABASE_URL,
+    env.SUPABASE_SERVICE_ROLE_KEY,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(req.headers.cookie ?? "");
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            res.appendHeader(
+              "Set-Cookie",
+              serializeCookieHeader(name, value, options),
+            ),
+          );
+        },
       },
     },
-  });
+  );
 };
