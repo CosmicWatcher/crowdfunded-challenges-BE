966ff75ed48f9ba9f50d24a86faa823ee06f47e2
CosmicWatcher <aryan.kh88@gmail.com>
2024-11-04 19:43:00 -0500

    db types update

---
+++ supabase/migrations/20241105003920_add_view_solution_votes_by_task_and_user.sql
@@ -0,0 +1,9 @@
+create view
+    public.solution_votes_by_task_and_user as
+select
+    solutions.task_id,
+    solution_votes.voted_by,
+    solution_votes.vote_count
+from
+    solutions
+    join solution_votes on solutions.id = solution_votes.solution_id;
