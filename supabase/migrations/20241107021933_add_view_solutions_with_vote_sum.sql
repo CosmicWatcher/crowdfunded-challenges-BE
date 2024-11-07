ce37ab9eac74d35a8d508e33f67f3b7b0cd7b9a8
CosmicWatcher <aryan.kh88@gmail.com>
2024-11-06 21:32:05 -0500

    added view solutions_with_vote_sum

---
+++ supabase/migrations/20241107021933_add_view_solutions_with_vote_sum.sql
@@ -0,0 +1,26 @@
+create view
+    public.solutions_with_vote_sum as
+select
+    solutions.id,
+    solutions.task_id,
+    solutions.created_by,
+    solutions.details,
+    solutions.is_winner,
+    solutions.created_at,
+    solutions.edited_at,
+    solutions.deleted_at,
+    solutions.title,
+    votes.sum
+from
+    solutions
+    join (
+        select
+            solution_votes.solution_id,
+            sum(solution_votes.vote_count) as sum
+        from
+            solution_votes
+        group by
+            solution_votes.solution_id
+    ) votes on solutions.id = votes.solution_id
+where
+    solutions.deleted_at is null;
