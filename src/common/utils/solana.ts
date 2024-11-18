a1e3a8d2089daf1d4f66015dd8fd9ee2ab76aabc
CosmicWatcher-Mac <>
2024-11-18 18:20:37 -0500

    make account rent exempt if it is not

--- src/common/utils/solana.ts
+++ src/common/utils/solana.ts
@@ -15,27 +15,38 @@ export const kinPubKey = new PublicKey(env.KIN_MINT_ADDRESS);
 export const solanaConn = new Connection(clusterApiUrl(env.SOLANA_RPC));
 
 export async function createAccountOnChain(newAccount: Keypair) {
-  const accountInfo = await solanaConn.getAccountInfo(newAccount.publicKey);
-  if (accountInfo) return;
-
   const space = 0;
-  const rentExemptionAmount =
-    await solanaConn.getMinimumBalanceForRentExemption(space);
 
-  const createAccountParams = {
-    fromPubkey: solanaPayer.publicKey,
-    newAccountPubkey: newAccount.publicKey,
-    lamports: rentExemptionAmount,
-    space,
-    programId: SystemProgram.programId,
-  };
-
-  const createAccountTransaction = new Transaction().add(
-    SystemProgram.createAccount(createAccountParams),
-  );
-
-  await sendAndConfirmTransaction(solanaConn, createAccountTransaction, [
-    solanaPayer,
-    newAccount,
+  const [rentExemptionAmount, accountInfo] = await Promise.all([
+    solanaConn.getMinimumBalanceForRentExemption(space),
+    solanaConn.getAccountInfo(newAccount.publicKey),
   ]);
+
+  if (accountInfo) {
+    if (accountInfo.lamports >= rentExemptionAmount) return;
+    const sendSolInstruction = SystemProgram.transfer({
+      fromPubkey: solanaPayer.publicKey,
+      toPubkey: newAccount.publicKey,
+      lamports: rentExemptionAmount - accountInfo.lamports,
+    });
+    const sendSolTransaction = new Transaction().add(sendSolInstruction);
+    await sendAndConfirmTransaction(solanaConn, sendSolTransaction, [
+      solanaPayer,
+    ]);
+  } else {
+    const createAccountParams = {
+      fromPubkey: solanaPayer.publicKey,
+      newAccountPubkey: newAccount.publicKey,
+      lamports: rentExemptionAmount,
+      space,
+      programId: SystemProgram.programId,
+    };
+    const createAccountTransaction = new Transaction().add(
+      SystemProgram.createAccount(createAccountParams),
+    );
+    await sendAndConfirmTransaction(solanaConn, createAccountTransaction, [
+      solanaPayer,
+      newAccount,
+    ]);
+  }
 }
